98ef59e1815c43b6824c683c129a608878b0cdc2
white <167047915+ha-yang9@users.noreply.github.com>
2024-05-01 18:04:01 +0900

    Update MainMinu.cs
    캐릭터 생성 기능 추가, 직업 선택 기능 추가

--- MainMinu.cs
+++ MainMinu.cs
@@ -49,10 +49,58 @@ namespace TRPGTest
         Shop shop = new Shop();
         Dungeon dungeon = new Dungeon();
         Rast rast = new Rast();
+        Player player = new Player();   //플레이어 객체 생성
 
+        public void CreatePlayer()  //사용자 이름 설정하기
+        {
+            Console.Clear();
+            Console.WriteLine("환영합니다! 캐릭터를 생성해주세요.");
+            Console.WriteLine("캐릭터 이름을 입력해주세요.");
+            Console.Write(">>");
+            string playerName = Console.ReadLine();
+            Console.WriteLine($"환영합니다, {playerName}님!");
+            player.Name = playerName;
+
+            string job = chooesJob();
+            player.Job = job;
+        }
+
+        public string chooesJob()   //직업 선택하기
+        {
+            string input = "";
+
+            Console.Clear();
+            Console.WriteLine("직업을 선택해주세요.\n");
+            Console.WriteLine("1. 전사");
+            Console.WriteLine("2. 궁수");
+            Console.WriteLine("3. 마법사\n");
+
+            Console.WriteLine("원하시는 행동을 입력해주세요");
+            Console.Write(">>");
+
+            input = Console.ReadLine();
+            switch (input)
+            {
+                case "1":
+                    Console.WriteLine("전사를 선택하셨습니다.");
+                    return "전사";
+                case "2":
+                    Console.WriteLine("궁수를 선택하셨습니다.");
+                    return "궁수";
+                case "3":
+                    Console.WriteLine("마법사를 선택하셨습니다.");
+                    return "마법사";
+                default:
+                    Console.WriteLine("잘못된 선택입니다.");
+                    return "";
+            }
+
+        }
 
         public void ShowMainMenu()
         {
+            CreatePlayer();
+
             string input = "";
             while (input != "0")
             {
@@ -68,7 +116,7 @@ $$ |  $$ |$$  __$$ |$$ | $$ | $$ |$$   ____|      $$\   $$ |  $$ |$$\ $$  __$$ |
                                                                                                                        
                                                                                                                        
                                                                                                                        ");                                          //시작할때 로고
-                Console.WriteLine("스파르타 마을에 오신 여러분 환영합니다.");
+                Console.WriteLine($"{player.Name}님! 스파르타 마을에 오신 여러분 환영합니다.");
                 Console.WriteLine("이곳에서 던전으로 들어가기 전 활동을 할 수 있습니다.\n");
                 Console.WriteLine("0. 게임종료");
                 Console.WriteLine("1. 상태 보기");
@@ -117,9 +165,6 @@ $$ |  $$ |$$  __$$ |$$ | $$ | $$ |$$   ____|      $$\   $$ |  $$ |$$\ $$  __$$ |
                 }
             }
         }
-        Player player = new Player(); // 플레이어 객체 생성
-
-
     }
 }
 
