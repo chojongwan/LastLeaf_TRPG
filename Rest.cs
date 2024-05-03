50f24d2974997ea26321a82a872a46f8ab938afa
강수지 <lemontea1321@naver.com>
2024-05-03 14:33:04 +0900

    [Fix] Status.cs, Rest.cs
    입력이 꼬이는 버그 수정, 오타 수정

--- Rest.cs
+++ Rest.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Numerics;
+using System.Runtime.ConstrainedExecution;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,43 +13,47 @@ namespace TRPGTest
         public void GoRest(Player player)
         {
             Console.Clear();
-            Console.WriteLine($"휴식하기");
-            Console.WriteLine($"500 G 를 내면 체력을 회복할 수 있습니다. (보유 골드 : {player.Gold} G)\n");
+            string input = "";
 
-            Console.WriteLine("1. 휴식하기");
-            Console.WriteLine("0. 나가기\n");
-
-            Console.WriteLine("원하시는 행동을 입력해주세요.");
-
-            string input = Console.ReadLine();
-
-            switch (input)
+            while (input != "0")
             {
-                case "1":
-                    if (player.Gold >= 500 && player.HP < 100)
-                    {
-                        player.Gold -= 500; // 골드 차감
-                        player.HP = Math.Min(player.HP + 100, 100); // 최대 체력 100으로 제한하여 회복
-                        Console.WriteLine("휴식을 완료했습니다.");
-                    }
-                    else if (player.Gold < 500)
-                    {
-                        Console.WriteLine("Gold 가 부족합니다.");
-                    }
-                    else if (player.HP >= 100)
-                    {
-                        Console.WriteLine("이미 체력이 최대입니다.");
-                    }
-                    break;
-                case "0":
-                    break;
-                default:
-                    Console.WriteLine("잘못된 입력입니다.");
-                    break;
-            }
+                Console.WriteLine("휴식하기");
+                Console.WriteLine($"500 G 를 내면 체력을 회복할 수 있습니다. (현재 체력: {player.HP}) (보유 골드: {player.Gold} G)\n");
 
-            Console.WriteLine("계속하려면 아무 키나 누르세요...");
-            Console.ReadKey();
+                Console.WriteLine("1. 휴식하기");
+                Console.WriteLine("0. 나가기\n");
+
+                while (input != "0")
+                {
+                    Console.Write("원하시는 행동을 입력해주세요.\n> ");
+                    input = Console.ReadLine();
+
+                    switch (input)
+                    {
+                        case "1":
+                            if (player.Gold < 500)
+                            {
+                                Console.WriteLine("Gold 가 부족합니다.");
+                            }
+                            else if (player.HP >= 100)
+                            {
+                                Console.WriteLine("이미 체력이 최대입니다.");
+                            }
+                            else
+                            {
+                                player.Gold -= 500; // 골드 차감
+                                player.HP = Math.Min(player.HP + 100, 100); // 최대 체력 100으로 제한하여 회복
+                                Console.WriteLine($"휴식을 완료했습니다. (현재 체력: {player.HP}) (보유 골드: {player.Gold} G)");
+                            }
+                            break;
+                        case "0":
+                            break;
+                        default:
+                            Console.WriteLine("잘못된 입력입니다.");
+                            break;
+                    }
+                }
+            }
         }
     }
 }
--- Status.cs
+++ Status.cs
@@ -16,35 +16,55 @@ namespace TRPGTest
             while (input != "0")
             {
                 Console.Clear();
-                Console.WriteLine($"LV: {player.LV}");
-                Console.WriteLine($"이름: {player.Name}");
-                Console.WriteLine($"직업 ({player.Job})");
+                Console.WriteLine("상태 보기");
+                Console.WriteLine("캐릭터의 정보가 표시됩니다.\n");
+
+                Console.WriteLine($"Lv: {player.LV.ToString("D2")}");
+                Console.WriteLine($"이름: {player.Name} (직업: {player.Job})");
                 Console.WriteLine($"공격력: {player.Attack}");
-                Console.WriteLine($"방여력: {player.Defense}");
+                Console.WriteLine($"방어력: {player.Defense}");
                 Console.WriteLine($"체력: {player.HP}");
                 Console.WriteLine($"마력: {player.MP}");
                 Console.WriteLine($"Gold: {player.Gold} G\n");
+                Console.WriteLine("-. 관리자 모드");
                 Console.WriteLine("0. 나가기\n");
-                Console.WriteLine("원하시는 행동을 입력해주세요.");
 
-                input = Console.ReadLine();
-                if (input != "0")
+                while (input != "0")
                 {
-                    Console.WriteLine("master code 입력.");
-                    string master = Console.ReadLine();
-                    if (master == "allmaster")
+                    Console.Write("원하시는 행동을 입력해주세요.\n> ");
+                    input = Console.ReadLine();
+
+                    switch (input)
                     {
-                        Console.WriteLine("비밀번호를 입력하세요.");
-                        string input1 = Console.ReadLine();
-                        if (input1 == "1004")
-                        {
-                            player.Attack = 1004;
-                            player.Defense = 1004;
-                        }
+                        case "0":
+                            break;
+                        case "-":
+                            string code = "";
+                            while (code != "0" && code != "allmaster")
+                            {
+                                Console.WriteLine("관리자 코드를 입력해주세요.");
+                                Console.WriteLine("0. 나가기");
+                                Console.Write("> ");
+                                code = Console.ReadLine();
+
+                                if (code == "allmaster")
+                                {
+                                    player.Gold = 100004;
+                                    player.Attack = 1004;
+                                    player.Defense = 1004;
+                                    Console.WriteLine("관리자 모드가 설정되었습니다.");
+                                }
+                                else if (code != "0")
+                                {
+                                    Console.WriteLine("코드가 다릅니다.");
+                                }
+                            }
+                            break;
+                        default:
+                            Console.WriteLine("잘못된 입력입니다.");
+                            break;
                     }
-                    Console.ReadKey();
                 }
-                
             }
         }
     }
